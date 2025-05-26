5dd7df07423ddec14ced928d79800319f5f4531a
zzangbbo06 <zzangbbo0623@gmail.com>
2025-05-26 14:17:26 +0900

    JAVA 상속 2번 과제
    실수한 부분들 주석 달았음

--- chapter06/src/com/gn/homework/inheritance/homework02/ChocolateController.java
+++ chapter06/src/com/gn/homework/inheritance/homework02/ChocolateController.java
@@ -5,9 +5,12 @@ public class ChocolateController{
 	
 	
 	public String bakeChocolateCake(double flour, double cream, int cherry, int chip) {
-		Chocolate c = new Chocolate(flour, cream, cherry,chip);
+		Chocolate c = new Chocolate(flour, cream, cherry,chip); 
+		// 여기서도 값을 받아줘야 Chocolate 객체에 값을 대입 가능
+		// 여기서 매개변수에 값을 넣어주지 않아서 값이 기본값인 0으로 계속 출력 되었음
 		System.out.println(c.toString());
-		return "초콜릿 케이크가 완성되었습니다";
+		return "초콜릿 케이크가 완성되었습니다"; 
+		// 이렇게 선언하면 호출하고자 하는 파일에서 변수에 넣어서 sysout 호출해주어야함.
 	}
 	
 }
