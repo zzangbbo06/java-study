f1dfcb8346aa2d03d1a10cde08a0147f184a42b9
zzangbbo0623 <zzangbbo0623@gmail.com>
2025-05-29 16:39:45 +0900

    Java 과제 Spring 클래스

--- chapter11/src/com/gn/Homework06.java
+++ chapter11/src/com/gn/Homework06.java
@@ -5,14 +5,53 @@ import java.util.Scanner;
 public class Homework06 {
 	public static void main(String[] args) {
 		String sentence = "the quick brown fox jumps over the lazy dog";
+		String[] sentenceSplit = sentence.split(" ");
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("단어를 입력하세요: ");
 		String checkWord = sc.nextLine();
+
+		boolean checked = false;
 		
-		if(checkWord.indexOf(sentence)) {
-			System.out.println();
+		for(int i = 0; i < sentenceSplit.length; i++) {
+			if(!sentenceSplit[i].equals(checkWord)) {
+				continue;
+			}else {
+				checked = true;
+				if(i + 1 < sentenceSplit.length) {
+					System.out.println(checkWord + " 다음 단어 : " + sentenceSplit[i + 1]);
+					continue;
+				}else {
+					System.out.println("마지막 단어입니다.");
+				}
+			}
+			
+		}		
+		if(checked == false) {
+			System.out.println("존재하지 않는 단어입니다.");	
 		}
 		
+		
+		
+		
+	
+		
+//		for(int i = 0; i < sentenceSplit.length; i++) {	
+//			if(sentenceSplit[i].equals(checkWord)) {
+//				
+//				if(i + 1 < sentenceSplit.length) {
+//					System.out.println(checkWord + " 다음 단어: " + sentenceSplit[i + 1]);
+//                } else {
+//                	System.out.println("입력한 단어가 마지막 단어입니다.");
+//                }
+//			}else {
+//				System.out.println("존재하지 않는 단어입니다.");
+//				break;
+//			}
+//		
+//		}
+//
+//		sc.close();
 	}
+	
 }
